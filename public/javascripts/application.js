0cba7a841393ca7f99f78e0afc2c2debae3e1281
Paul Campbell <paul@rslw.com>
2010-10-21 12:43:32 +0100

    tasty truncation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -104,6 +104,57 @@ $(document).ready(function(){
   })
   
   $("div#sorting-endorsements").hide()
+  
+  function linkify(text){
+      if (text) {
+          text = text.replace(
+              /((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,
+              function(url){
+                  var full_url = url;
+                  if (!full_url.match('^https?:\/\/')) {
+                      full_url = 'http://' + full_url;
+                  }
+                  return '<a href="' + full_url + '">' + url + '</a>';
+              }
+          );
+      }
+      return text;
+  }
+
+  $(function(){
+    $('ul.tweet-list').each(function(){
+      var ul = $(this);
+      var twitter_username = ul.attr('data-username');
+      $.jTwitter(twitter_username, 3, function(posts){
+        for(var i=0; i<posts.length; i++){
+          ul.append('<li class="tweet-item">'  + linkify(posts[i].text) +  '</li>');
+        }
+        return false;
+      });
+      return false;
+    })
+
+  });
+  
+  $('ul.search-details').click(function(){
+    h3 = $(this).prev('h3:first');
+    if(h3.attr('data-original-value') === void(0))
+    {
+      h3.attr('data-original-value', h3.text())
+    }
+    var selected_items = [];
+    $(this).find('input:checked').each(function(){
+      selected_items.push($('label[for=' + $(this).attr('id') + ']').text())
+    })
+    if(selected_items.length === 0)
+    {
+      h3.text(h3.attr('data-original-value'));
+    }
+    else
+    {
+      h3.text($.string(selected_items.join(', ')).truncate(20).str);
+    }
+  })
 })
 
 var RecaptchaOptions = {
