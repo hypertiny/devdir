03f6090a08bfd1d8934fafd0c4f1a2663b9e2b0f
Paul Campbell <paul@rslw.com>
2010-11-26 11:17:31 +0000

    first steps to Et

--- public/javascripts/application-ext.js
+++ public/javascripts/application-ext.js
@@ -4,5 +4,21 @@ Ext.BLANK_IMAGE_URL = '/ext/resources/images/default/s.gif';
 // Main application entry point
 Ext.onReady(function() {
 // write your application here
-  console.info('woohoo!!!');
+  function textarea_maxlength(e){
+    var textarea = Ext.get(e.target)
+    remaining = textarea.dom.attributes['data-count'].value - textarea.getValue().length
+    console.log(remaining)
+    Ext.select('em.number').update(remaining + '')
+    if(remaining >= 0)
+    {
+        Ext.select('.number').removeClass('over-limit')
+    }
+    else
+    {
+      Ext.select('.number').addClass('over-limit')
+    }
+  }
+  
+  Ext.select('textarea[data-count]').on('keyup',textarea_maxlength)
+  Ext.select('textarea[data-count]').on('change',textarea_maxlength)
 });
