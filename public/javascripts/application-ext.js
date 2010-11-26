7aa8bcff68357e02b23dbfa2572381e726653bde
Paul Campbell <paul@rslw.com>
2010-11-26 10:48:11 +0000

    bootstrap ext

---
+++ public/javascripts/application-ext.js
@@ -0,0 +1,8 @@
+// Path to the blank image must point to a valid location on your server
+Ext.BLANK_IMAGE_URL = '/ext/resources/images/default/s.gif';
+
+// Main application entry point
+Ext.onReady(function() {
+// write your application here
+  console.info('woohoo!!!');
+});
