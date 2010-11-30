bc9277707050873a87d7e2111da513e3418e89b4
Jamie Avins <jamieavins@jamie.local>
2010-11-30 12:07:38 -0800

    Update some paths for production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,11 @@
-role :app, "staging.sencha.com"
-role :web, "staging.sencha.com"
-role :db,  "staging.sencha.com", :primary => true
+role :app, "web1.sencha.com"
+role :web, "web1.sencha.com"
+role :db,  "web1.sencha.com", :primary => true
 set :rails_env, 'production'
 set :branch, "master"
+set :deploy_to, "/mnt/ruby/hypertiny#{application}"
+set :user, "ubuntu"
+set :group, "ubuntu"
 
 namespace :deploy do
   desc "Deploy!" 
