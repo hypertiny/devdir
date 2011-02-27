f4f4444f448da954626a61ed25136d234088a58b
James Pearce <github@tripleodeon.com>
2011-02-27 08:47:26 -0800

    Endorsement strife sorted out

--- app/controllers/my/endorsements_controller.rb
+++ app/controllers/my/endorsements_controller.rb
@@ -1,13 +1,13 @@
 class My::EndorsementsController < ApplicationController
-  
+
   before_filter :require_user
-  
+
   def index
     @provider = current_user.provider
     @endorsement_requests = @provider.endorsement_requests.all
     @endorsements = @provider.endorsements
   end
-  
+
   def sort
     params[:endorsement].each_with_index do |endorsement_id, index|
       current_user.provider.endorsements.find(endorsement_id).update_attribute(:sort_order, index)
@@ -22,22 +22,22 @@ class My::EndorsementsController < ApplicationController
     @provider = current_user.provider
     @endorsement = Endorsement.find(params[:id])
   end
-  
+
   def update
     @endorsement = current_user.provider.endorsements.find(params[:id])
     @provider = current_user.provider
-    
+
     @endorsement.aasm_state = params[:state]
     if @endorsement.save
       flash[:notice] = t('endorsement.saved_successfully')
     end
     redirect_to [:my, @endorsement]
   end
-  
+
   def update_all
     @endorsements = current_user.provider.endorsements.find(params[:endorsement_ids])
     @endorsements.each do |endorsement|
-      if params[:endorsements][endorsement.id.to_s] and params[:endorsements][endorsement.id.to_s][:approved]
+      if params[:endorsements] and params[:endorsements][endorsement.id.to_s] and params[:endorsements][endorsement.id.to_s][:approved]
         endorsement.approve!
       else
         endorsement.reject!
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  
+
   # User routes
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
@@ -7,31 +7,31 @@ ActionController::Routing::Routes.draw do |map|
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.resource :session
   map.resources :password_resets
-  
+
   # Country and state routes
   map.resources :top_cities
-  
+
   Country.slugs.each do |slug|
     map.send(slug.gsub('-', '_'), "/developers/#{slug}", :controller => "countries", :action => "show", :country => slug)
   end
-  
+
   State.slugs.each do |slug|
     map.send(slug.gsub('-', '_'), "/developers/us-#{slug}", :controller => "states", :action => "show", :state => slug)
   end
-  
+
   # App routes
   map.resource :home, :controller => 'home'
   map.resource :provider_directory, :as => :developer_directory, :controller => "provider_directory"
-  
+
   map.resources :favorites
   map.resources :providers, :as => :developers, :shallow => true, :collection => {:search => :get, :by_location => :get} do |provider|
     provider.resources :endorsements
     provider.resources :portfolio_items
     provider.resources :favorites
-  end  
+  end
   map.resources :rfps
   map.resources :pages, :requirements => { :id => /.*/}
-  
+
   # Provider admin routes
   map.my '/my', :controller => 'my/dashboard', :action => 'show'
   map.namespace :my do |me|
@@ -40,10 +40,10 @@ ActionController::Routing::Routes.draw do |map|
     me.resources :portfolio_items
     me.resources :endorsement_requests
     me.resources :users
-    me.resources :endorsements, :collection => {:sort => :put, :update_all => :put}
+    me.resources :endorsements, :collection => {:sort => :put, :update_all => :post}
     me.resources :requests
   end
-  
+
   # Admin routes
   map.admin '/admin', :controller => 'admin/dashboard', :action => 'show'
   map.change_password '/admin/change_password', :controller => 'admin/users', :action => 'change_password'
